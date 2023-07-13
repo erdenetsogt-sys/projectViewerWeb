5cdcaf9d587543af06552078b1326de40ebfcfe0
erdenetsogt-sys <tsogt1225@gmail.com>
2023-07-13 22:38:22 +0900

    local branch

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const colors = require("colors");
+const { graphqlHTTP } = require("express-graphql");
+require("dotenv").config();
+const connectDB = require("./config/db");
+const schema = require("./schema/schema");
+const port = process.env.PORT || 5000;
+const app = express();
+
+//Connect to db
+connectDB();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV == "development",
+  })
+);
+app.listen(port, console.log(` running on ${port}`));
