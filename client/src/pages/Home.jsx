22824f8a590494fea017073f262e3d7a34181513
erdenetsogt-sys <tsogt1225@gmail.com>
2023-07-16 23:52:12 +0900

    Able to add create a project

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import Clients from "../components/Clients";
 import AddClientModal from "../components/AddClientModal";
 import Projects from "../components/Projects";
 import React from "react";
+import AddProjectModal from "../components/addProjectModel";
 
 export default function Home() {
   return (
     <>
       <div className="d-flex gap-3 mb-4">
         <AddClientModal />
+        <AddProjectModal />
       </div>
       <Projects />
 
