ba036adb3c3533845f10ecf0eb369de30d633a29
erdenetsogt-sys <tsogt1225@gmail.com>
2023-08-13 17:02:32 +0900

    change in clientrow.jsx

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -7,18 +7,8 @@ import { GET_PROJECTS } from "../queries/projectQueries";
 export default function ClientRow({ client }) {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    //catch again
     refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
-    // update(cache, { data: { deleteClient } }) {
-    //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
-    //   cache.writeQuery({
-    //     query: GET_CLIENTS,
-    //     data: {
-    //       clients: clients.filter((c) => c.id !== deleteClient.id),
-    //     },
-    // });
   });
-  // });
   return (
     <tr>
       <td>{client.name}</td>
